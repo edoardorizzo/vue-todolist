0fc5a6ee949f9d291984d5ca49ba9bcf3f8c4547
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-30 16:16:38 +0200

    create Ui of todolist, add vuejs

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,32 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      tasks: [
+        {
+            text: 'Design header sito beauty',
+            done: true
+        },
+        {
+            text: 'Call revisione sito Mountain Bike',
+            done: false
+        },
+        {
+            text: 'Call allineamneto team di sviluppo',
+            done: false
+        },
+        {
+            text: 'Design component card',
+            done: false
+        },
+        {
+            text: 'Design carousel',
+            done: false
+        },
+        {
+            text: 'Design comp 4/8 col',
+            done: false
+        },
+      ]
     }
   }
 }).mount('#app')
