7b6126670c3d6a99fcfde0c01bfab118d04bfb89
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-30 17:36:54 +0200

    push

--- assets/app.js
+++ assets/app.js
@@ -6,29 +6,44 @@ createApp({
         inputText: '',
         tasks: [
         {
-            text: 'Design header sito beauty'
+            text: 'Design header sito beauty',
+            done: false
         },
         {
-            text: 'Call revisione sito Mountain Bike'
+            text: 'Call revisione sito Mountain Bike',
+            done: false
         },
         {
-            text: 'Call allineamneto team di sviluppo'
+            text: 'Call allineamneto team di sviluppo',
+            done: false
         },
         {
-            text: 'Design component card'
+            text: 'Design component card',
+            done: false
         },
         {
-            text: 'Design carousel'
+            text: 'Design carousel',
+            done: false
         },
         {
-            text: 'Design comp 4/8 col'
+            text: 'Design comp 4/8 col',
+            done: false
         },
       ]
     }
   },
   methods: {
     addTaskToList(){
-        this.tasks.unshift(this.inputText);
+        this.tasks.unshift(this.inputText)
+    },
+    taskDone(){
+        this.tasks.done = true;
+        console.log(this.taskDone);
+    },
+    lineThrough(){
+        if (this.tasks.done == true) {
+            this.tasks.text
+        }
     }
   }
 }).mount('#app')
